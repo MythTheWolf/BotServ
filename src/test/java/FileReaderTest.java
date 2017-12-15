8926063f2825b83a2781fdb0a3698e71a65a1851
MythTheWolf <myth.wolf6@gmail.com>
2017-12-15 17:41:09 -0600

    Updated the unit test

--- src/test/java/FileReaderTest.java
+++
@@ -1,8 +0,0 @@
-import org.junit.Test;
-
-public class FileReaderTest {
-	@Test
-	public void testFile() {
-		System.out.println("USER DIR---->" + System.getProperty("user.dir"));
-	}
-}
---
+++ src/test/java/SystemStartupTest.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import com.myththewolf.BotServ.BotServ;
+import com.myththewolf.BotServ.lib.tool.Utils;
+
+public class SystemStartupTest {
+	@Test
+	public void startSystem() {
+		String TOKEN = Utils.readFile(new File("JunitTestToken.txt"));
+		JSONObject config = new JSONObject();
+		config.put("token", TOKEN);
+		config.put("repos", new JSONArray());
+		File RUN = new File("run");
+		RUN.mkdirs();
+		Utils.writeToFile(config.toString(), new File("run/settings.json"));
+		String[] args = { "--noterm", "--nopkg" };
+		BotServ.main(args);
+	}
+}
