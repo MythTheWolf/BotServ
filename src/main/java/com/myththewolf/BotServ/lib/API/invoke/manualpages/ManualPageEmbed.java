c592448eba27b650954e8f6f52b9f67339e3ad90
MythTheWolf <myth.wolf6@gmail.com>
2018-01-07 22:43:22 -0600

    Show the default

--- src/main/java/com/myththewolf/BotServ/lib/API/invoke/manualpages/ManualPageEmbed.java
+++ src/main/java/com/myththewolf/BotServ/lib/API/invoke/manualpages/ManualPageEmbed.java
@@ -51,31 +51,27 @@ public class ManualPageEmbed {
     }
 
     public void decremntPage() throws IllegalArgumentException, ParseException {
+        if (page == 0) {
+            page = 0;
+            EmbedBuilder EB = new EmbedBuilder();
+            EB.setTitle("Manual Page: " + mp.getName());
+
+            EB.setColor(Color.magenta);
+            EB.addField("DESCRIPTION", "```" + mp.getDescription() + "```", false);
+            EB.addField("USAGE", "```" + mp.getSyn() + "```", false);
+            EB.addField("INDEX", "```" + mp.getIndex() + "```", false);
+            msg.editMessage(EB.build()).queue();
+            return;
+        }
+
         if ((page - 1 < 0)) {
             page = MAX_PAGES;
         } else {
             page--;
         }
-        String FOOTER = mp.IP.paramExists("@page " + page, "footer") ? (String) mp.IP.getParamsOf("@page " + page).get("footer")
-                : mp.pl.getPluginName();
-        Color C = (mp.IP.paramExists("@page " + page, "color")
-                && ((String) mp.IP.getParamsOf("@page " + page).get("color")).split(",").length == 3)
-                ? new Color(Integer.parseInt(((String) mp.IP.getParamsOf("@page " + page).get("color")).split(",")[0]),
-                Integer.parseInt(((String) mp.IP.getParamsOf("@page " + page).get("color")).split(",")[1]),
-                Integer.parseInt(((String) mp.IP.getParamsOf("@page " + page).get("color")).split(",")[2]))
-                : Color.CYAN;
-
-
         EmbedBuilder EB = new EmbedBuilder();
         EB.setTitle("Manual page: " + page + "/" + MAX_PAGES);
         EB.setDescription(mp.getPage(page));
-        EB.setFooter(FOOTER, null);
-        EB.setColor(C);
-        if (mp.IP.getParamsOf("@page " + page).get("field") instanceof List<?>) {
-            ((List<String>) mp.IP.getParamsOf("@page " + page).get("field")).forEach(con -> {
-
-            });
-        }
         msg.editMessage(EB.build()).queue();
 
     }
